08f3cddc583c6a052ef6f6e1c057821f4b6e20a7
Ethan <ethan@etekweb.net>
2020-05-07 19:27:21 -0500

    updated config and removed router

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
     ? '/simple-weather/'
-    : '/'
+    : '/',
+
+    pwa: {
+      name: 'Simple Weather',
+      themeColor: '#4A90E2',
+      msTileColor: '#4A90E2',
+      manifestOptions: {
+        background_color: '#4A90E2'
+      }
+    }
 }
