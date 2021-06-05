96c6eff518a05a5682fb16ab6fbac42240450198
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-06-05 22:14:47 +0000

    Deploy etekweb/clear-skies to github.com/etekweb/clear-skies.git:gh-pages

---
+++ precache-manifest.5c88622dfa98b71fd201503af5fb01cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2981788bcecf2180169e",
+    "url": "/clear-skies/css/app.a458ade4.css"
+  },
+  {
+    "revision": "00681e6663472a9079a3cdd35d516032",
+    "url": "/clear-skies/index.html"
+  },
+  {
+    "revision": "2981788bcecf2180169e",
+    "url": "/clear-skies/js/app.6a725d58.js"
+  },
+  {
+    "revision": "9743fc76cca1b7bfc8ce",
+    "url": "/clear-skies/js/chunk-vendors.6cd98b66.js"
+  },
+  {
+    "revision": "843f62bfefe892007836d75796e31fd1",
+    "url": "/clear-skies/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/clear-skies/robots.txt"
+  }
+]);
